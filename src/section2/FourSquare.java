d29935f9c6bf4a884f0044d86e75f3df789516a0
League Student <league@iMac-23.attlocal.net>
2019-10-11 18:26:48 -0700

    hi

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -5,28 +5,84 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
+	//
+	Robot hi = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		hi.setSpeed(1000000000);
 		// 5. Set the pen width to 5
-
+		hi.setPenWidth(10000);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for(int i = 0; i < 4; i++) {
+			
+		
 
 			// 7. Set the pen color to random
+		hi.setRandomPenColor();
 	
 			// 1. Call the drawSquare() method
+	drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+             hi.turn(90);
+		}
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		JOptionPane.showMessageDialog(null, "no!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		hi.penDown();
+		hi.move(100);
+		hi.setRandomPenColor();
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+	
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,59 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot hi = new Robot();
+		hi.setRandomPenColor();
+		hi.setSpeed(1000000000);
+		hi.penDown();
+		hi.move(100);
+		hi.setRandomPenColor();
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.setRandomPenColor();
+		hi.move(100);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
+		hi.turn(2000000000);
 	
-		
 		
 	}
 }
